f4ffc0b8b3a267138ad50457736ad229cccc1212
quinn <maninios@163.com>
2020-12-03 12:34:13 +0800

    [SK]增加使用示例

--- Example/CTNet/ViewController.swift
+++ Example/CTNet/ViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 import CTNet
+import RxSwift
+import NSObject_Rx
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,32 +21,85 @@ class ViewController: UIViewController {
         CTNetConfigure.shared.timeout = 20
         CTNetConfigure.shared.HTTPEvaluators = ["192.168.11.147"]
         
+        /// codable
+        UserNetManger.userInfo(usrid: "123").map { (json) -> UserInfo? in
+            return (self.toModel(UserInfo.self, value: json))
+        }.subscribe { (user) in
+            print(user?.message ?? "")
+        } onError: { (error) in
+            print(error.localizedDescription)
+        } onCompleted: {
+            print("完成")
+        } onDisposed: {
+            print("销毁")
+        }.disposed(by: rx.disposeBag)
         
-        // Do any additional setup after loading the view, typically from a nib.
+        /// json
+        UserNetManger.userInfo(usrid: "123").subscribe { (json) in
+            print(json)
+        } onError: { (error) in
+            print(error.localizedDescription)
+        } onCompleted: {
+            print("完成")
+        } onDisposed: {
+            print("销毁")
+        }.disposed(by: rx.disposeBag)
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    /// 字典转模型
+    func toModel<T>(_ type: T.Type, value: Any?) -> T? where T: Decodable {
+        guard let value = value else { return nil }
+        return toModel(type, value: value)
+    }
+    /// 字典转模型
+    func toModel<T>(_ type: T.Type, value: Any) -> T? where T : Decodable {
+        guard let data = try? JSONSerialization.data(withJSONObject: value) else { return nil }
+        let decoder = JSONDecoder()
+        decoder.nonConformingFloatDecodingStrategy = .convertFromString(positiveInfinity: "+Infinity", negativeInfinity: "-Infinity", nan: "NaN")
+        return try? decoder.decode(type, from: data)
+    }
+    
+}
+struct UserInfo:Codable {
+    var message:String
 }
 
-struct NetManger{
+class UserNetManger:NetBaseManger{
+    static func userInfo(usrid:String)->Observable<[String: Any]>{
+        return request(parameters: ["id":usrid], url: "/ping", cacheID: "/ping", method: .get)
+    }
+}
 
-//    static func userInfo(usrid:String)->Single<[String: Any]?>{
-//        let single: Single<[String: Any]?> = Single.create { single in
-//            CTNet.request(url: "/ping", method: .get, parameters: [:]) { (jsonDict, error) in
-//                if let _error = error{
-//                    let myError = NSError(domain: _error.msg, code: _error.code, userInfo: nil)
-//                    single(.error(myError))
-//                }else{
-//                    single(.success(jsonDict))
-//                }
-//            }
-//            return Disposables.create()
-//        }
-//        return single
-//    }
- 
+class NetBaseManger{
+    static func request(parameters:[String:Any],url:String,cacheID:String,method:CTNetRequestMethod = .post)->Observable<[String: Any]>{
+        let observable: Observable<[String: Any]> = Observable.create { observable in
+            CTNet.request(url: url, method: method, parameters: [:], level: .high, cacheID: cacheID) { (jsonDict) in
+                if var myJsonDict = jsonDict{
+                    myJsonDict["isCache"] = true
+                    observable.onNext(myJsonDict)
+                }
+            } netCallBack: { (jsonDict, error) in
+                if let _error = error{
+                    let myError = NSError(domain: _error.msg, code: _error.code, userInfo: nil)
+                    observable.onError(myError)
+                }else{
+                    if let myJsonDict = jsonDict{
+                        observable.onNext(myJsonDict)
+                    }else{
+                        let myError = NSError(domain: "data is error", code: -1300, userInfo: nil)
+                        observable.onError(myError)
+                    }
+                }
+            }
+            return Disposables.create()
+        }
+        return observable
+    }
 }
--- Example/Pods/Target Support Files/Pods-CTNet_Example/Pods-CTNet_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-CTNet_Example/Pods-CTNet_Example-frameworks.sh
@@ -177,10 +177,22 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CTNet/CTNet.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Differentiator/Differentiator.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NSObject+Rx/NSObject_Rx.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxDataSources/RxDataSources.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CTNet/CTNet.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Differentiator/Differentiator.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NSObject+Rx/NSObject_Rx.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxDataSources/RxDataSources.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
